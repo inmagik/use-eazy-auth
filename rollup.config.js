79a2ba0118605ba0ec1a95721d90f9d5dd7c9507
gffuma <fumagalli.gf@gmail.com>
2020-10-27 17:11:15 +0100

    Actualize build

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,47 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import pkg from './package.json'
 
 const vendors = []
   // Make all external dependencies to be exclude from rollup
   .concat(
     Object.keys(pkg.dependencies || {}),
-    Object.keys(pkg.peerDependencies || {}),
-    'rxjs/operators',
+    Object.keys(pkg.peerDependencies || {})
   )
 
-export default ['esm', 'cjs'].map(format => ({
+const makeExternalPredicate = (externalArr) => {
+  if (externalArr.length === 0) {
+    return () => false
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`)
+  return (id) => pattern.test(id)
+}
+
+export default ['esm', 'cjs'].map((format) => ({
   input: {
-    'index': 'src/index.js',
-    'routes': 'src/routes/index.js',
+    index: 'src/index.js',
+    routes: 'src/routes/index.js',
   },
   output: [
     {
       dir: 'lib',
       entryFileNames: '[name].[format].js',
       exports: 'named',
-      format
-    }
+      format,
+    },
   ],
-  external: vendors,
-  plugins: [babel({ exclude: 'node_modules/**' })],
+  external: makeExternalPredicate(vendors),
+  plugins: [
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            useESModules: format === 'esm',
+          },
+        ],
+      ],
+    }),
+  ],
 }))
