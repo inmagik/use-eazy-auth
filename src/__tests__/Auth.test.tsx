c392c57598ab4e3c7cd5f5239b9c50bd15738202
gffuma <fumagalli.gf@gmail.com>
2020-12-22 20:07:42 +0100

    Tests for updateUser and patchUser

--- src/__tests__/Auth.test.tsx
+++ src/__tests__/Auth.test.tsx
@@ -94,7 +94,7 @@ describe('Auth', () => {
       writable: true,
     })
 
-    const AuthWrapper = ({ children } : { children: ReactNode }) => (
+    const AuthWrapper = ({ children }: { children: ReactNode }) => (
       <Auth loginCall={loginCall} meCall={meCall}>
         {children}
       </Auth>
@@ -147,5 +147,138 @@ describe('Auth', () => {
 
     // Token for ma caller
     expect(fakeApi).toHaveBeenCalledWith(99)
+  })
+
+  it('Should give an action to updateUser', async () => {
+    // Fake da calls
+    const loginCall = jest.fn()
+    // Hack for manual resolve the me promise
+    let resolveMe: TestCallBack<DummyUser>
+    const meCall = jest.fn(
+      () =>
+        new Promise<DummyUser>((resolve) => {
+          resolveMe = resolve
+        })
+    )
+
+    // Fake a good storage
+    const resolvesGetItem: TestCallBack[] = []
+    const localStorageMock = {
+      getItem: jest.fn(() => new Promise((r) => resolvesGetItem.push(r))),
+      setItem: jest.fn(),
+      removeItem: jest.fn(),
+    }
+    Object.defineProperty(global, '_localStorage', {
+      value: localStorageMock,
+      writable: true,
+    })
+
+    const AuthWrapper = ({ children }: { children: ReactNode }) => (
+      <Auth loginCall={loginCall} meCall={meCall}>
+        {children}
+      </Auth>
+    )
+
+    function useAllAuth() {
+      return {
+        user: useAuthUser<DummyUser, number>(),
+        actions: useAuthActions<number, never, DummyUser>(),
+      }
+    }
+
+    const { result } = renderHook(() => useAllAuth(), {
+      wrapper: AuthWrapper,
+    })
+
+    await act(async () => {
+      resolvesGetItem[0](JSON.stringify({ accessToken: 23 }))
+    })
+
+    await act(async () => {
+      resolveMe({ username: 'Giova' })
+    })
+
+    expect(result.current.user).toEqual({
+      user: { username: 'Giova' },
+      token: 23,
+    })
+
+    await act(async () => {
+      result.current.actions.updateUser({ username: 'Boundman!' })
+    })
+
+    expect(result.current.user).toEqual({
+      user: { username: 'Boundman!' },
+      token: 23,
+    })
+  })
+
+  it('Should give an action to patchUser', async () => {
+    interface AgedUser {
+      username: string
+      age: number
+    }
+
+    // Fake da calls
+    const loginCall = jest.fn()
+    // Hack for manual resolve the me promise
+    let resolveMe: TestCallBack<AgedUser>
+    const meCall = jest.fn(
+      () =>
+        new Promise<AgedUser>((resolve) => {
+          resolveMe = resolve
+        })
+    )
+
+    // Fake a good storage
+    const resolvesGetItem: TestCallBack[] = []
+    const localStorageMock = {
+      getItem: jest.fn(() => new Promise((r) => resolvesGetItem.push(r))),
+      setItem: jest.fn(),
+      removeItem: jest.fn(),
+    }
+    Object.defineProperty(global, '_localStorage', {
+      value: localStorageMock,
+      writable: true,
+    })
+
+    const AuthWrapper = ({ children }: { children: ReactNode }) => (
+      <Auth loginCall={loginCall} meCall={meCall}>
+        {children}
+      </Auth>
+    )
+
+    function useAllAuth() {
+      return {
+        user: useAuthUser<AgedUser, number>(),
+        actions: useAuthActions<number, never, AgedUser>(),
+      }
+    }
+
+    const { result } = renderHook(() => useAllAuth(), {
+      wrapper: AuthWrapper,
+    })
+
+    await act(async () => {
+      resolvesGetItem[0](JSON.stringify({ accessToken: 23 }))
+    })
+
+    await act(async () => {
+      resolveMe({ username: 'Giova', age: 23 })
+    })
+
+    expect(result.current.user).toEqual({
+      user: { username: 'Giova', age: 23 },
+      token: 23,
+    })
+
+    await act(async () => {
+      result.current.actions.patchUser({ age: 99 })
+    })
+
+    expect(result.current.user).toEqual({
+      user: { username: 'Giova', age: 99 },
+      token: 23,
+    })
   })
 })
